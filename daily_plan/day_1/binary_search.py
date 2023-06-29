dcf7fbd0a9c6bd27d05e4cee14d7c2d4a3ac27ee
David Mejia <davidfdomejiab@gmail.com>
2023-06-28 20:22:25 -0500

    basic binary search implemented

---
+++ daily_plan/day_1/binary_search.py
@@ -0,0 +1,23 @@
+def search(self, nums: List[int], target: int) -> int:
+    if(len(nums) < 1): return False
+    if(len(nums) == 1):
+        if(nums[0] == target): return 0
+    if(len(nums) == 2):
+        if(nums[0] == target): return 0
+        if(nums[1] and nums[1] == target): return 1
+
+
+    left = 0
+    right = len(nums) - 1
+
+    while(left <= right):
+        mid = left + (right - left) // 2
+
+        if(nums[mid] == target): return mid
+
+        if(target > nums[mid]):
+            left = mid + 1
+        else:
+            right = mid - 1
+
+    return -1
