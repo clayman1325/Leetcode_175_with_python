543f00ad96c6f68fda782bf39ceaf65d45eb405f
David Mejia <davidfdomejiab@gmail.com>
2023-06-30 12:04:23 -0500

    use fast and slow transverse of a linked list to detect a cycle in a Linked list

---
+++ daily_plan/day_3/linked_list_hasCycle.py
@@ -0,0 +1,9 @@
+def hasCycle(self, head: Optional[ListNode]) -> bool:
+    fast = head
+    slow = head
+
+    while(fast and fast.next and fast.next.next):
+        fast = fast.next.next
+        slow = slow.next
+        if(fast == slow): return True
+    return False
