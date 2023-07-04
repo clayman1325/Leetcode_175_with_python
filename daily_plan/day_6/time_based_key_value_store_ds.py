35e8aaa6f6651422ef40fc58afb1e14bbfe8136b
David Mejia <davidfdomejiab@gmail.com>
2023-07-03 20:16:45 -0500

    clean print statements in key-value data structure

--- daily_plan/day_6/time_based_key_value_store_ds.py
+++ daily_plan/day_6/time_based_key_value_store_ds.py
@@ -1,5 +1,4 @@
 class TimeMap:
-
     def __init__(self):
         self.store = {}
 
@@ -18,12 +17,10 @@ class TimeMap:
         if(values[0][0] > timestamp): return ""
 
         if(values[len(values) - 1][0] <= timestamp): return values[len(values) - 1][1]
-        print("david mejia")
+
         for idx in range(len(values)):
             value = values[idx]
             next_idx = idx + 1
             if(next_idx == len(values)): next_idx = len(values) - 1
             next_value = values[next_idx]
-            print(timestamp, value[0], next_value[0])
-            print(value[0] >= timestamp, value[1])
             if(next_value[0] > timestamp and value[0]<= timestamp): return value[1]
