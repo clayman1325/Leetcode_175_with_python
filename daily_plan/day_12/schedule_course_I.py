234d36ec5dbfbee2ff12159539d84c051a06b15e
David Mejia <davidfdomejiab@gmail.com>
2023-07-10 17:29:52 -0500

    solve the posibility to take all course in a set of n courses with its pre requisites

---
+++ daily_plan/day_12/schedule_course_I.py
@@ -0,0 +1,25 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        adj = {}
+        for num in range(numCourses):
+            adj[num] =  []
+        for requisite in prerequisites:
+            adj[requisite[0]].append(requisite[1])
+        print(adj)
+        for num in range(numCourses):
+            test_course = num
+            visited = set()
+            q = [num]
+
+            while(q):
+                cur_course = q.pop()
+
+                for next_course in adj[cur_course]:
+                    if adj[next_course] == True:
+                        q = []
+                        break
+                    if next_course in visited: return False
+                    visited.add(next_course)
+                    q.append(next_course)
+            adj[num] = True
+        return True
