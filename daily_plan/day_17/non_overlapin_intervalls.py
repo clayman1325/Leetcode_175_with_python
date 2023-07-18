75bdb5c3d5c747fc2de9651200c91a5738b92e30
David Mejia <davidfdomejiab@gmail.com>
2023-07-18 10:49:12 -0500

    remove print instruction

--- daily_plan/day_17/non_overlapin_intervalls.py
+++ daily_plan/day_17/non_overlapin_intervalls.py
@@ -2,7 +2,7 @@ class Solution:
     def eraseOverlapIntervals(self, intervals: List[List[int]]) -> int:
         k = -1000000
         intervals.sort(key= lambda prop: prop[1])
-        print(intervals)
+
         count = 0
         for i in range(len(intervals)):
             start = intervals[i][0]
